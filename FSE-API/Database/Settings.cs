83aa0368653a6c94104d536486a1c39566833b76
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-22 16:33:47 +0530

    Added RavenDB extensions

---
+++ FSE-API/Database/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FSE_API.Database
+{
+    [ExcludeFromCodeCoverage]
+    public class Settings
+    {
+        public RavenDB RavenDB { get; set; }
+    }
+    [ExcludeFromCodeCoverage]
+    public class RavenDB
+    {
+        public string[] Urls { get; set; }
+        public string DatabaseName { get; set; }
+        public string CertPath { get; set; }
+        public string CertPass { get; set; }
+    }
+}
