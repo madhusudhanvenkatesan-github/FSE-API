50f6d5e5896a78c8d8ff6e5777b96063df5879f6
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-22 15:57:47 +0530

    added List task

---
+++ FSE-API/DBMessages/ListTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace FSE_API.DBMessages
+{
+    public class ListTask
+    {
+        public string ProjectId { get; set; }
+        public string TaskId { get; set; }
+        public string TaskDescription { get; set; }
+        public string ParentTaskId { get; set; }
+        public string ParentDescription { get; set; }
+        public int Priority { get; set; }
+        public int Status { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string TaskOwnerId { get; set; }
+        public string TaskOwnerName { get; set; }
+    }
+}
