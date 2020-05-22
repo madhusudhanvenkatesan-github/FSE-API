4470bde481e674d669f805a36ac37414d61c5e55
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-22 16:07:15 +0530

    Added addusermsg

---
+++ FSE-API/DBMessages/AddUserMsg.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FSE_API.DBMessages
+{
+    public class AddUserMsg
+    {
+        [Required]
+        public string EmployeeId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
