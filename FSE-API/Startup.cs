dbcbf9a0d55e8f6b13cfaad892fca6f5a483c46c
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-23 00:46:05 +0530

    Updated startup and program files & added dockerfile

--- FSE-API/Startup.cs
+++ FSE-API/Startup.cs
@@ -1,18 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using FSE_API.Utilities;
+using FSE_API.Service;
+using FSE_API.Database;
+using FSE_API.Repository;
 
 namespace FSE_API
 {
+    [ExcludeFromCodeCoverage]
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -25,7 +25,24 @@ namespace FSE_API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                    .AddJsonOptions(options =>
+                    {
+                        options.JsonSerializerOptions.Converters.Add(new DateTimeConverter());
+                    });
+            services.AddAsyncDocumentSession(Configuration);
+            services.AddScoped<IUser, User>();
+            services.AddScoped<IProjectTask, ProjectTask>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ITaskProjectService, TaskProjectService>();
+            services.AddCors(corsOption => corsOption.AddPolicy("ReactPolicy", builder =>
+            {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyMethod();
+                builder.AllowAnyHeader();
+
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
