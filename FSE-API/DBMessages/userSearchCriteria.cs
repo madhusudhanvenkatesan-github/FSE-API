cdda17cfc5af5baed2c2d91cbd790e23a9ca19cf
madhusudhanvenkatesan <madhusudhan.venkatesan@cognizant.com>
2020-05-23 21:31:39 +0530

    deleting files

--- FSE-API/DBMessages/userSearchCriteria.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FSE_API.DBMessages
-{
-    public class userSearchCriteria
-    {
-        public string EmployeeID { get; set; }
-        public string LastName { get; set; }
-        public string FirstName { get; set; }
-    }
-}
--- FSE-API/Model/UserTaskVO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FSE_API.Model
-{
-    public class UserTaskVO
-    {
-        public TaskProject Tasks { get; set; }
-        public string UserName { get; set; }
-        public string ProjectId { get; set; }
-    }
-}
